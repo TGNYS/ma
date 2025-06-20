7c7d5b6cf281f2bf8f416d7d97528f3cccafd4bd
TGNYS <anyianyi8888@qq.com>
2025-06-21 01:00:03 +0800

    更新 落叶中心fa源码 (1) (1).lua

--- 落叶中心fa源码 (1) (1).lua
+++ 落叶中心fa源码 (1) (1).lua
@@ -1,18 +1,3 @@
-function Notify(Title1, Text1, Icon1, Time1)
-  game:GetService("StarterGui"):SetCore("SendNotification", {
-    Title = Title1,
-    Text = Text1,
-    Icon = Icon1,
-    Duration = Time1,
-  })
-end
-Notify("落叶中心", "作者krX", "rbxassetid://17360377302", 3)
-Notify("永久免费", "祝你玩的开心","rbxassetid://17360377302",3)
-Notify("永久免费", "没有盈利","rbxassetid://17360377302",3)
-Notify("落叶中心", "永久免费","rbxassetid://17360377302",3)
-Notify("拒绝跑路", "拒绝倒卖","rbxassetid://17360377302",3)
-Notify("启动完成", "祝你玩的开心","rbxassetid://17360377302",5)
-
 local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/XiaoYunCN/UWU/main/Library%2FSilent%20ui'))()
 local Window = Library:new("落叶中心 fa");
 
